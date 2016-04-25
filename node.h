524a08bb69f2a017e1c9d0c8bc053a917f092350
x-zho14 <616702857@qq.com>
2016-04-25 15:30:17 +0800

    Added files via upload

--- node.h
+++ node.h
@@ -8,6 +8,7 @@ public:
 	Node();
 	double priority(double c);
 	void pass_up();
+	void pass_up(int _n,int _result);
 	int corr_child(const int lastY);
 	int best_child(double c);
 	void Node::print_child();
@@ -16,10 +17,11 @@ public:
 	int n;
 	int result;
 	int index;
-	bool haschild;
-	bool mustwin;
 	int stone_posx;
 	int stone_posy;
+	bool haschild;
+	bool mustwin;
+	bool musttie;
 };
 
 #endif
